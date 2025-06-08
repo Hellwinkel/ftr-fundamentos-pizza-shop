8f98a1fbc79c97d3c5b10012ed0c0485dc8a0389
Hellwinkel <guirovedah@gmail.com>
2025-06-07 23:25:57 -0300

    Páginas e componentes (UI) - Notificações toast (sonner)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 import './index.css'
 import { router } from './routes'
 
 export function App() {
-    return <RouterProvider router={router} />
+    return (
+        <>
+            <Toaster richColors />
+            <RouterProvider router={router} />
+        </>
+    )
 }
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 const signInForm = z.object({
@@ -18,9 +19,23 @@ export function SignIn() {
     } = useForm<SignInForm>()
 
     async function handleSignIn(data: SignInForm) {
-        console.log(data)
+        try {
+            console.log(data)
 
-        await new Promise((resolve) => setTimeout(resolve, 2000))
+            await new Promise((resolve) => setTimeout(resolve, 2000))
+
+            toast.success(
+                'Enviamos um link de autenticação para o seu e-mail',
+                {
+                    action: {
+                        label: 'Reenviar',
+                        onClick: () => handleSignIn(data),
+                    },
+                }
+            )
+        } catch (error) {
+            toast.error('Credenciais inválidas')
+        }
     }
 
     return (
