82ca8c03789253796f87d20d79123e84936ccccb
Hellwinkel <guirovedah@gmail.com>
2025-06-08 20:56:37 -0300

    Páginas e componentes (UI) - Página: Cadastro de restaurante

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -40,6 +41,14 @@ export function SignIn() {
 
     return (
         <div className='p-8'>
+            <Button
+                asChild
+                variant={'ghost'}
+                className='absolute right-8 top-8'
+            >
+                <Link to='/sign-up'>Novo estabelecimento</Link>
+            </Button>
+
             <div className='w-[350px] flex flex-col justify-center gap-6'>
                 <div className='flex flex-col gap-2 text-center'>
                     <h1 className='text-2xl font-semibold tracking-tight'>
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 import { AppLayout } from './pages/layouts/app'
 import { AuthLayout } from './pages/layouts/auth'
 
@@ -22,6 +23,10 @@ export const router = createBrowserRouter([
             {
                 path: '/sign-in',
                 element: <SignIn />,
+            },
+            {
+                path: '/sign-up',
+                element: <SignUp />,
             },
         ],
     },
