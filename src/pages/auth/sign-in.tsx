74f16f689ebfa1f602583043c8f07d736f550927
Hellwinkel <guirovedah@gmail.com>
2025-06-07 23:08:04 -0300

    Páginas e componentes (UI) - Usando React Hook Form

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,8 +1,28 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const signInForm = z.object({
+    email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
+    const {
+        register,
+        handleSubmit,
+        formState: { isSubmitting },
+    } = useForm<SignInForm>()
+
+    async function handleSignIn(data: SignInForm) {
+        console.log(data)
+
+        await new Promise((resolve) => setTimeout(resolve, 2000))
+    }
+
     return (
         <div className='p-8'>
             <div className='w-[350px] flex flex-col justify-center gap-6'>
@@ -15,13 +35,20 @@ export function SignIn() {
                     </p>
                 </div>
 
-                <form className='space-y-4'>
+                <form
+                    onSubmit={handleSubmit(handleSignIn)}
+                    className='space-y-4'
+                >
                     <div className='space-y-2'>
                         <Label htmlFor='email'>Seu e-mail</Label>
-                        <Input id='email' type='email' />
+                        <Input id='email' type='email' {...register('email')} />
                     </div>
 
-                    <Button type='submit' className='w-full'>
+                    <Button
+                        type='submit'
+                        className='w-full'
+                        disabled={isSubmitting}
+                    >
                         Acessar painel
                     </Button>
                 </form>
