e9a3237567432e546a87eec7455adf3c07e18b0b
Hellwinkel <guirovedah@gmail.com>
2025-06-09 22:58:01 -0300

    Páginas e componentes (UI) - ThemeToggle e menu da conta

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from './components/theme/theme-provider'
 import './index.css'
 import { router } from './routes'
 
 export function App() {
     return (
-        <>
+        <ThemeProvider storageKey='pizzashop-theme' defaultTheme='system'>
             <Toaster richColors />
             <RouterProvider router={router} />
-        </>
+        </ThemeProvider>
     )
 }
