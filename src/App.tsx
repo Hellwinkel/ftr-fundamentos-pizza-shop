b9149f6fd9959c2be2dcb7b40789a52654a77229
Hellwinkel <guirovedah@gmail.com>
2025-06-18 20:19:48 -0300

    Conexão com a API - Autenticação do usuário

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { QueryClientProvider } from 'react-query'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 import { ThemeProvider } from './components/theme/theme-provider'
 import './index.css'
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
-    return (
-        <ThemeProvider storageKey='pizzashop-theme' defaultTheme='system'>
-            <Toaster richColors />
-            <RouterProvider router={router} />
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider storageKey='pizzashop-theme' defaultTheme='system'>
+      <Toaster richColors />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ThemeProvider>
+  )
 }
