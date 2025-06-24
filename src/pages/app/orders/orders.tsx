bd4faea8ae2e55840bba4de0b937db32a2d68ce7
Hellwinkel <guirovedah@gmail.com>
2025-06-23 22:05:23 -0300

    Conexão com a API - Filtro dos pedidos

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -10,11 +10,15 @@ import {
 import { useQuery } from 'react-query'
 import { useSearchParams } from 'react-router-dom'
 import { z } from 'zod'
-import { OrderTableFilter } from './order-table-filter'
+import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
+
+  const orderId = searchParams.get('orderId')
+  const customerName = searchParams.get('customerName')
+  const status = searchParams.get('status')
 
   const pageIndex = z.coerce
     .number()
@@ -22,8 +26,14 @@ export function Orders() {
     .parse(searchParams.get('page') ?? '1')
 
   const { data: result } = useQuery({
-    queryKey: ['orders', pageIndex],
-    queryFn: () => getOrders({ pageIndex }),
+    queryKey: ['orders', pageIndex, orderId, customerName, status],
+    queryFn: () =>
+      getOrders({
+        pageIndex,
+        orderId,
+        customerName,
+        status: status === 'all' ? null : status,
+      }),
   })
 
   function handlePaginate(pageIndex: number) {
@@ -40,7 +50,7 @@ export function Orders() {
         <h1 className='text-3xl font-bold tracking-tight'>Pedidos</h1>
       </div>
       <div className='space-y-2.5'>
-        <OrderTableFilter />
+        <OrderTableFilters />
 
         <div className='border rounded-md'>
           <Table>
